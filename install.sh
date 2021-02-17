cb50139a11970a1a260e49e430d2c8f1a419b90a
AGRU13 <kush9453@gmail.com>
2021-02-17 13:54:30 +0530

    install.h

--- install.sh
+++ install.sh
@@ -1,21 +1,22 @@
-# commands for mongodb installation
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+#command for node
+sudo apt update
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+# commands for mongodb installation
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
 sudo apt update
 
 sudo apt install -y mongodb-org
 
-sudo systemctl start mongod
+sudo systemctl start mongod.service
 
 sudo systemctl status mongod
 
 sudo systemctl enable mongod
 
-#installing nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-
-. ~/.nvm/nvm.sh
-
-nvm install node
+mongo --eval 'db.runCommand({ connectionStatus: 1 })'
